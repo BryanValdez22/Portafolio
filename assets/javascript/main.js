6bfeda2fa7939003028c91cc2aafdd70eac81198
Brayan Valdez Jaraba <jaravaldezbryan2016@gmail.com>
2022-12-24 17:43:48 -0500

    Proyecto Web Portafolio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,10 @@
 const menuToggle = document.querySelector('.toggle');
 const navegation = document.querySelector('.nav');
-const blob = document.querySelector('.blob');
+
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active')
     navegation.classList.toggle('active')
-    blob.classList.toggle('active')
 }
+setTimeout(()=>{
+    document.querySelector('.page__loading').remove()
+},3000)
