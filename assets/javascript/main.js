cbf2bae145ca4dd7e3340c444fb9a13af88e0dec
Brayan Valdez Jaraba <jaravaldezbryan2016@gmail.com>
2022-12-26 09:53:38 -0500

    Proyecto Portafolio Web

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,3 +8,23 @@ menuToggle.onclick = function(){
 setTimeout(()=>{
     document.querySelector('.page__loading').remove()
 },3000)
+
+// Send Mail
+window.onload = function() {
+    document.querySelector('form').addEventListener('submit', function(event){
+        event.preventDefault();
+
+        this.name = document.querySelector('#name').value,
+        this.last = document.querySelector('#last').value,
+        this.email = document.querySelector('#email').value,
+        this.message = document.querySelector('#message').value
+
+        emailjs.sendForm('service_x9pnt13','template_nk4mbdo', this)
+        .then(function(){
+            alert("mensaje enviado");
+        }, function(error){
+            console.error('error...', error);
+        });
+        document.querySelector('form').reset();
+    })
+}
